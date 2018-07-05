8c34c3cdb9159a1db7f8fdfaace87544fac85f43
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-05 05:09:30 +0000

    change seat css

--- public/js/test.js
+++ public/js/test.js
@@ -1,12 +1,8 @@
 $(function () {
     $('.seat1').click(function(){
+        $('.seats_index').hide();
         $('.offence').fadeIn();
-        $('.seat1').css('background-color', 'blue');
-        $('.cover').css('background-color', 'rgba(0, 0, 0, 0.3)');
     });
     
-    $('.game_choice').click(function(){
-        $('.game_index').fadeIn();
-    })
-    
+   
 })
