742d9902ed9291a24aa7a1678c752e52ae680a63
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-10 20:03:20 +0000

    modified navbar

---
+++ resources/views/commons/navbar3.blade.php
@@ -0,0 +1,20 @@
+<header>
+  <div id="nav-drawer">
+      <input id="nav-input" type="checkbox" class="nav-unshown">
+      <label id="nav-open" for="nav-input"><span></span></label>
+      <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+      <div id="nav-content">
+        <ul>
+          @if (Auth::check())
+            <li><a href="#"><span class="nav-text">Timeline</span></a></li>
+            <li><a href="{{ route('seat', ['id' => $user->id]) }}"><span class="nav-text">Seats</span></a></li>
+            <li><a href="{{ route('users.show', ['id' => $user->id])}}"><span class="nav-text">My Page</span></a></li>
+            <li><a href="{{ route('logout.get') }}"><span class="nav-text">Logout</span></a></li>
+          @else
+            <li><a href="{{ route('signup.get') }}"><span class="nav-text">Sign Up</span></a></li>
+            <li><a href="{{ route('login') }}"><span class="nav-text">Log in</span></a></li>
+          @endif
+      </ul>
+    </div>
+  </div>
+</header>
