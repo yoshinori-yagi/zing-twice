a2ce36f3da5a75b9296b749c0ed21c90117603a4
jiro.kirameki <ec2-user@ip-172-31-41-166.us-east-2.compute.internal>
2018-07-10 01:10:38 +0000

    offence controller 1~45

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+                    <div class ="offence">
+                        <h1>team name{{D   }}を攻める！！</h1>
+                        <a href="" class="btn btn-default btn-ghost btn-lg game_choice">Profile</a>
+                        <a href="{{ route('game.index', ['id' => $user->id]) }}" class="btn btn-default btn-ghost btn-lg game_choice">Gameを選ぶ</a>
+                        <a href="" class="btn btn-default btn-ghost btn-lg close_button">閉じる</a>
+                    </div>
+
+
+
+
+?>
