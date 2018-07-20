54b962dab2bbf9373609ca5aa066fa8d504d93f9
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-20 06:18:11 +0000

    a liitle

--- resources/views/games/block_defence.blade.php
+++ resources/views/games/block_defence.blade.php
@@ -10,7 +10,7 @@
                 <button id="disp" onclick="main()" class ="btn-lg">START</button>
                 <a href="{{ route('block.game_result', ['id' => $user->id]) }}" class="btn btn-ghost btn-lg game_finish tetoris_fininsh">Send this result</a>
             </div>
-            <a><img src="{{ secure_asset("images/character.jpg") }}" alt="" class="character"></a>
+            <a><img src="{{ secure_asset("images/character.png") }}" alt="" class="character"></a>
             <h3 class="information">explanation</h3>
         </div>
 @endsection
