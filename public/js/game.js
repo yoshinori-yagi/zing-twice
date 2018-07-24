64f6d05aaf9d9e8c8300adc13ebf325347604adf
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-24 14:40:59 +0000

    seat responsive

--- public/js/game.js
+++ public/js/game.js
@@ -978,6 +978,34 @@ function saveBlockScore() {
 /* Jquery */
 
 $(function() {
+    
+    $('tribe_index_red').click(function() {
+        $('tribe_red').show();
+        $('tribe_yellow').hide();
+        $('tribe_pink').hide();
+        $('tribe_purple').hide();
+    });
+    
+    $('tribe_index_yellow').click(function() {
+        $('tribe_yellow').fadeIn();
+        $('tribe_red').hide();
+        $('tribe_pink').hide();
+        $('tribe_purple').hide();
+    });
+    
+    $('tribe_index_pink').click(function() {
+        $('tribe_pink').show();
+        $('tribe_yellow').hide();
+        $('tribe_red').hide();
+        $('tribe_purple').hide();
+    });
+    
+    $('tribe_index_purple').click(function() {
+        $('tribe_purple').show();
+        $('tribe_yellow').hide();
+        $('tribe_pink').hide();
+        $('tribe_red').hide();
+    });
 
 /* Slide show*/
   function toggleChangeBtn() {
@@ -1957,6 +1985,8 @@ $(function() {
             $('.purple').css('background-color','white').css('box-shadow', '1px 2px MediumOrchid')
         }
     );
+    
+    
     
     
     $('.seats_mini').hover(
