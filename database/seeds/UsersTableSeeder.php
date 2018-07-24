4f27586109914811918b912d1833920add01bab3
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-24 04:19:32 +0000

    seeding2

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,231 +14,231 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
         [
           'name' => 'youはたまご',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'BoB',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Fortune',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Ra Ra Ra RIZAP',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'ぽなるけみり',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'The U.K',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Siesta',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Kolkata',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'TWICE',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Etarnal Dragon',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => '卍Olive卍',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'なだ万',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => '',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'kara-age',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Porsche',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'TERAKOYA',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Y .Nature',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Amigos',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Letian panda',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Avengers',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'CodeWars',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Pandanity',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Daichi+no+Megumi',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'YAKITOMATO',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'HRM',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'SAKEty',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Kiwami Pudding',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => '御富士も梅雨',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Team One',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],[
           'name' => 'Team Sekaikan',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'JIAOZI',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'TK Kingdom',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Jasmine',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'いいとも',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Mkpali',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Pocket 6',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'チームきりんさん',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'koders',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'JETS',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => '新鮮グミ',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Medium Rare',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'ひよこさん組',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'A BANANA',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Noisyy',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'Team Kay',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         [
           'name' => 'kemkow',
-          'password' => becrypt('123456'),
+          'password' => bcrypt('123456'),
           
         ],
         
