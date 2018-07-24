8e2b94df7f16637eaab0bd01155e0dd6dd5fcdae
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-24 06:37:29 +0000

    navbar

--- resources/views/commons/navbar3.blade.php
+++ resources/views/commons/navbar3.blade.php
@@ -6,9 +6,9 @@
       <div id="nav-content">
         <ul>
           @if (Auth::check())
-            <li><a href="{{ route('seat', ['id' => $user->id]) }}"><span class="nav-text">Seats</span></a></li>
-            <li><a href="{{ route('users.show', ['id' => $user->id])}}"><span class="nav-text">My Page</span></a></li>
-            <li><a href="{{ route('users.buy', ['id' => $user->id])}}"><span class="nav-text">Buy points</span></a></li>
+            <li><a href="{{ route('seat', ['id' => \Auth::id()]) }}"><span class="nav-text">Seats</span></a></li>
+            <li><a href="{{ route('users.show', ['id' => \Auth::id()])}}"><span class="nav-text">My Page <?php if(\Auth::user()->notification !=0){print '❶';} ?></span></a></li>
+            <li><a href="{{ route('users.buy', ['id' => \Auth::id()])}}"><span class="nav-text">Buy points</span></a></li>
             <li><a href="{{ route('logout.get') }}"><span class="nav-text">Log out</span></a></li>
           @else
             <li><a href="/"><span class="nav-text">Home</span></a></li>
