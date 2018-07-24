c6c1240402e9f2f8ca71b07dc09274d3e6e3b639
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-24 05:11:14 +0000

    update

--- resources/views/games/block_result_after.blade.php
+++ resources/views/games/block_result_after.blade.php
@@ -6,7 +6,7 @@
                 <h2>game result</h2>
                 @if ($user_id_score > $team_id_score)
                 <h1 class = "result">Win, seat change</h1>
-                <a href="{{ route('seat.update', ['id' => $user->id]) }}" class="btn btn-ghost btn-lg">See updated seats</a>
+                <a href="{{ route('seat', ['id' => $user->id]) }}" class="btn btn-ghost btn-lg">See updated seats</a>
                 <a><img src="{{ secure_asset("images/character.jpg") }}" alt="" class="character"></a>
                 <h3 class="information">おめでとう！勝負に勝ったみたいじゃ！</h3>
                 @else
