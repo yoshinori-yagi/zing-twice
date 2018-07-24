8380d33aa1dc7c56f73d0f65e7887138bc280055
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-24 04:17:18 +0000

    seeding

---
+++ database/seeds/SeatsTableSeeder.php
@@ -0,0 +1,152 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SeatsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('seats')->insert([
+        [
+          'team_id' => '1',
+        ],
+        [
+          'team_id' => '2',
+        ],
+        [
+          'team_id' => '3',
+        ],
+        [
+          'team_id' => '4',
+        ],
+        [
+          'team_id' => '5',
+        ],
+        [
+          'team_id' => '6',
+        ],
+        [
+          'team_id' => '7',
+        ],
+        [
+          'team_id' => '8',
+        ],
+        [
+          'team_id' => '9',
+        ],
+        [
+          'team_id' => '10',
+        ],
+        [
+          'team_id' => '11',
+        ],
+        [
+          'team_id' => '12',
+        ],
+        [
+          'team_id' => '13',
+        ],
+        [
+          'team_id' => '14',
+        ],
+        [
+          'team_id' => '15',
+        ],
+        [
+          'team_id' => '16',
+        ],
+        [
+          'team_id' => '17',
+        ],
+        [
+          'team_id' => '18',
+        ],
+        [
+          'team_id' => '19',
+        ],
+        [
+          'team_id' => '20',
+        ],
+        [
+          'team_id' => '21',
+        ],
+        [
+          'team_id' => '22',
+        ],
+        [
+          'team_id' => '23',
+        ],
+        [
+          'team_id' => '24',
+        ],
+        [
+          'team_id' => '25',
+        ],
+        [
+          'team_id' => '26',
+        ],
+        [
+          'team_id' => '27',
+        ],
+        [
+          'team_id' => '28',
+        ],
+        [
+          'team_id' => '29',
+        ],
+        [
+          'team_id' => '30',
+        ],
+        [
+          'team_id' => '31',
+        ],
+        [
+          'team_id' => '32',
+        ],
+        [
+          'team_id' => '33',
+        ],
+        [
+          'team_id' => '34',
+        ],
+        [
+          'team_id' => '35',
+        ],
+        [
+          'team_id' => '36',
+        ],
+        [
+          'team_id' => '37',
+        ],
+        [
+          'team_id' => '38',
+        ],
+        [
+          'team_id' => '39',
+        ],
+        [
+          'team_id' => '40',
+        ],
+        [
+          'team_id' => '41',
+        ],
+        [
+          'team_id' => '42',
+        ],
+        [
+          'team_id' => '43',
+        ],
+        [
+          'team_id' => '44',
+        ],
+        [
+          'team_id' => '45',
+        ],
+      ]);
+    }
+}
