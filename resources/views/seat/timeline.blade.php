8a159986450db56a6a68e6b573e356368021685a
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-24 04:43:48 +0000

    timeline routing

--- resources/views/seat/timeline.blade.php
+++ resources/views/seat/timeline.blade.php
@@ -6,7 +6,7 @@
                 <h2>Game Now</h2>
                 <h3 class="timeline">{{ $user_name }} vs {{ $team_name }}</h3>
                 <h2>please wait</h2>
-                <a href="{{ route('seat', ['id' => $user->id])}}" class="btn btn-ghost btn-lg">Back</a>
+                <a href="{{ route('users.show', ['id' => $user->id])}}" class="btn btn-ghost btn-lg">My Page</a>
                 <a><img src="{{ secure_asset("images/character.jpg") }}" alt="" class="character"></a>
                 <h2 class="information">マイページにいくのじゃ</h2>
         </div>
