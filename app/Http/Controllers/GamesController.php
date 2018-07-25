c66b8e51a3cf7f85a32d3ea75aa07fdeb2193cfe
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-07-25 04:36:02 +0000

    number

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -167,6 +167,13 @@ class GamesController extends Controller
         
         DB::table('games')->where ('id',intval($game_id))->update(['team_id_score' => $number]);
         
+        DB::table('users')->update(['notification' => 0]);
+        
+        $user_id = DB::table('games')->orderby('id', 'desc')->select('games.user_id')->first();
+        $user_id = $user_id->user_id;
+        
+        DB::table('users')->where ('id',intval($user_id))->update(['notification' => 2]);
+        
         return view('games.defence',$data);                                
         
     }
@@ -180,13 +187,6 @@ class GamesController extends Controller
         
         $team_id_score = DB::table('games')->orderby('id', 'desc')->select('games.team_id_score')->first();
         $team_id_score = $team_id_score->team_id_score;
-        
-        DB::table('users')->update(['notification' => 0]);
-        
-        $user_id = DB::table('games')->orderby('id', 'desc')->select('games.user_id')->first();
-        $user_id = $user_id->user_id;
-        
-        DB::table('users')->where ('id',intval($user_id))->update(['notification' => 2]);
 
         $data = [
             'user' => $user,
